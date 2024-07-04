bf4374645ddfc59061690123c2f179434a126e56
Jules Follmer <julie.follmer@gmail.com>
2024-07-03 19:49:58 -0500

    update my versions of calculator

---
+++ calculator_my_v2_0.py
@@ -0,0 +1,107 @@
+"""Calculator version 2.0
+I wanted to have fun with the calculator program from assignment 
+7. 'Walk-through: Calculator' and give it extra features.
+
+
+Modifications to make:
+    - handle divide by zero
+        DONE
+    - make if-else block into a function
+        DONE
+    - handle words entered for operations
+        DONE
+    - drop the decimal in ouput if .0
+        DONE
+    - handle non-digit characters for numbers (causes error now)
+        DONE but can be nicer (fixed in v3)
+    - call check_divide() inside calculate()
+        DONE
+    - convert words to numbers (for fun, more advanced handling of 
+      strings)
+        NOT DONE for this version
+Done with this version. See calculator_my_v3.py for more changes.
+"""
+
+
+print('Welcome to Calculator!')
+
+
+# Get a number and check for correct format:
+def get_number():
+    number = input()
+
+    # handle non-digit inputs for numbers:
+    for char in number:
+        if not (char.isdigit() or char == '.'):
+            print('Unsupported format. Please enter a number (may include a '
+                  'negative sign and/or a decimal):  ', end='')
+            return get_number()
+    return float(number)
+
+# Get an operation and check for correct format:
+def get_operation():
+    operation = input()
+
+    add = ['1', '1)', 'add', 'addition', '+', 'plus']
+    subtract = ['2', '2)', 'subtract', 'subtraction', '-', 'minus']
+    multiply = ['3', '3)', 'multiply', 'multiplication', '*', 'times']
+    divide = ['4', '4)', 'divide', 'division', '/', 'divided by', 'divided']
+
+    if operation.casefold() in add:
+        return 'add'
+    elif operation.casefold() in subtract:
+        return 'subtract'
+    elif operation.casefold() in multiply:
+        return 'multiply'
+    elif operation.casefold() in divide:
+        return 'divide'
+    else:
+        print("Operation type not supported.\nPlease enter the number, name, "
+              "or symbol of the operation you wish to perform.\n1. Add (+)  "
+              "2. Subtract (-)  3. Multiply (*)  4. Divide (/):  ", end='')
+        return get_operation()
+
+
+# Ask the user for the first number.
+print("What's the first number?:   ", end='')
+number1 = get_number()
+
+# Ask the user for the second number.
+print("What's the second number?:  ", end='')
+number2 = get_number()
+
+# Ask the user for an operation to perform.
+print("What operation would you like to perform?\n"
+      "1) Add (+)  2) Subtract (-)  3. Multiply (*)  4. Divide (/):  ", 
+      end='')
+operation = get_operation()
+
+
+# handle divide by zero:
+def check_divide(number2):
+    if number2 == 0:
+        print("Can't divide by zero. Enter a new second number:  ", end='')
+        return get_number()
+    return number2
+
+# Perform the operation on the two numbers and return the result.
+def calculate(number1, number2, operation):
+    if operation == 'add':
+        return number1 + number2
+    elif operation == 'subtract':
+        return number1 - number2
+    elif operation == 'multiply':
+        return number1 * number2
+    elif operation == 'divide':
+        number2 = check_divide(number2)
+        return number1 / number2
+
+
+# Format the result to make the decimals look nicer:
+def format_output(result):
+    return int(result) if result % 1 == 0 else result
+
+# Run calculation and and print to terminal.
+result = format_output(calculate(number1, number2, operation))
+print(f"The result is:\n"
+      f"{result}")
---
+++ calculator_my_v2_1.py
@@ -0,0 +1,151 @@
+"""Calculator version 2.1
+I wanted to have fun with the calculator program from assignment 
+7. 'Walk-through: Calculator' and give it extra features. This version
+was done after assignment 11. 'Walk-through: Refactoring Calculator'.
+
+
+Modifications to make (from v2):
+    - handle divide by zero
+        DONE
+    - make if-else block into a function
+        DONE
+    - handle words entered for operations
+        DONE
+    - drop the decimal in ouput if .0
+        DONE
+    - handle non-digit characters for numbers
+        DONE
+    - convert words to numbers (for fun, more advanced handling of 
+      strings)
+        NOT DONE in this version
+More modifications as per assignment 11:
+    - create a unique terminal prompt
+        DONE
+    - make if-else block a match-case block
+        DONE for calculate() but not get_operation() (doesn't work with 
+        'in')
+    - ask if user wants to perform another calculation and start over if 
+      so
+        NOT DONE in this version
+More modifications I'd like:
+    - rewrite check_validity() using a try-except block
+        DONE
+    - nested functions don't actually need to be nested functions
+        DONE
+    - remove check_divide() and just use its code inside calculate()
+        DONE
+    - support power and square root operations
+        DONE
+"""
+
+from math import sqrt
+
+# Unique terminal prompt:
+# Use prompt() instead of print()
+def prompt(message):
+    print(f"==> {message}")
+
+# Use input_prompt() instead of input()
+def input_prompt():
+    string = input("==>: ")
+    return string
+
+prompt('Welcome to Calculator!')
+
+# Get a number, and if not correct format, get a new one:
+def get_number():
+    number = input_prompt()
+
+    try:
+        float(number)
+    except ValueError:
+        prompt("Unsupported format. Accepted characters include numbers 0-9, "
+               "a negative sign (-), and a decimal (.). Please enter a new "
+               "second number.")
+        return get_number()
+
+    return float(number)
+
+# Accept various operation inputs:
+add = ['1', '1)', 'add', 'addition', '+', 'plus']
+subtract = ['2', '2)', '-', 'subtract', 'subtraction', 'minus']
+multiply = ['3', '3)', '*', 'multiply', 'multiplication', 'times']
+divide = ['4', '4)', '/', 'divide', 'division', 'divided by', 'divided']
+power = ['5', '5)', '^', '**' 'power', 'to the power of']
+sqroot = ['6', '6)', '√', 'sqrt', 'square root', 'squareroot', 'root',
+          'sqroot']
+
+# Get an operation, and if not correct format, get a new one:
+def get_operation():
+    prompt("1) Add (+)  2) Subtract (-)  3. Multiply (*)  4. Divide (/)")
+    prompt("5) Power (^ or **)  6) Square Root (√)")
+    operation = input_prompt()
+
+    if operation.casefold() in add:
+        return 'add'
+    elif operation.casefold() in subtract:
+        return 'subtract'
+    elif operation.casefold() in multiply:
+        return 'multiply'
+    elif operation.casefold() in divide:
+        return 'divide'
+    elif operation.casefold() in power:
+        return 'power'
+    elif operation.casefold() in sqroot:
+        return 'sqroot'
+    else:
+        prompt("Not a valid operation type.")
+        prompt("Please enter the number, name, or symbol of the operation:")
+        return get_operation()
+
+# Get number(s) and the operation, plus some logic if divide or sqroot:
+def get_inputs():
+
+    # Ask the user for the first number.
+    prompt("What's the first number?")
+    number1 = get_number()
+
+    # Ask the user for an operation to perform.
+    prompt("What operation would you like to perform?")
+    operation = get_operation()
+
+    if operation == 'sqroot':
+        return (number1, None, operation)
+
+    else:
+        # Ask the user for the second number.
+        prompt("What's the second number?")
+        number2 = get_number()
+
+        # Don't divide by zero:
+        if operation == 'divide' and number2 == 0:
+            prompt("Can't divide by zero. Please enter a new second number.")
+            number2 = get_number()
+
+        return (number1, number2, operation)
+
+number1, number2, operation = get_inputs()
+
+# Perform the operation, and don't divide by zero:
+def calculate(number1, number2, operation):
+    match operation:
+        case 'add':
+            return number1 + number2
+        case 'subtract':
+            return number1 - number2
+        case 'multiply':
+            return number1 * number2
+        case 'divide':
+            return number1 / number2
+        case 'power':
+            return number1**number2
+        case 'sqroot':
+            return sqrt(number1)
+
+# Format the result to make the decimals look nicer:
+def format_output(result):
+    return int(result) if result % 1 == 0 else result
+
+# Run calculation and and print to terminal.
+result = format_output(calculate(number1, number2, operation))
+prompt(f"The result is: {result}")
