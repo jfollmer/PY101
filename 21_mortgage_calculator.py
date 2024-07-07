908421ef3c1edf93af2744dfcf3e03b2a3491d07
Jules Follmer <julie.follmer@gmail.com>
2024-07-06 21:47:52 -0500

    update assignments

--- 21_mortgage_calculator.py
+++ 21_mortgage_calculator.py
@@ -13,10 +13,12 @@ To Do:
     - accept commas in numbers
         DONE
     - add commas back into results
-        DONE but do this in a different way, str.format() maybe
+        DONE but do this in a different way, str.format() maybe. Given
+             solution uses f"{payment:2f}"
     - handle balloon loans
     - run through pylint
-    International support:
+        DONE but do again if you change anything
+    - International support:
         - language support
         - accept commas as decimal points and periods as denomination
           separators
@@ -38,17 +40,19 @@ while True:
     def input_prompt():
         string = input("==>: ")
 
-        # don't accept excessively long numbers
-        if len(string) > 15:
+        # don't accept excessively long input:
+        if len(string) > 16:
             prompt("Input is too long. Please try again:")
             string = input_prompt()
 
         return string
 
-    # Print welcome message
-    prompt("Welcome to Mortgage / Car Loan Calculator!")
+    # Print welcome message:
+    prompt("|--------------------------------------------|")
+    prompt("| Welcome to Mortgage / Car Loan Calculator! |")
+    prompt("|--------------------------------------------|")
 
-    # Ask for loan amount
+    # Ask for loan amount and check input for validity:
     def get_loan_amount():
 
         # currency = # use UTC codes
@@ -60,32 +64,51 @@ while True:
             amount = Dec(amount)
             if amount < 0:
                 prompt("Please enter a positive number:")
-                get_loan_amount()
+                amount = get_loan_amount()
+            if len(str(amount % 1)) > 2:
+                prompt("Fractional cents not accepted.")
+                prompt("Please enter whole cents. Example: 4999.99:")
+                amount = get_loan_amount()
         except decimal.InvalidOperation:
             prompt("Please enter a number.")
             prompt("You may optionally include a decimal and commas.")
-            get_loan_amount()
+            amount = get_loan_amount()
 
         return amount
 
-    amount = get_loan_amount()
+    loan_amount = get_loan_amount()
 
-    # Ask for APR
-    def get_apr():
+    # Ask for APR and check input for validity:
+    def get_apr_to_mpr():
         prompt("What is the annual percentage rate (APR)?")
-        apr = input_prompt()
+        rate = input_prompt()
+        
+        # This needs to be outside of this function or written differently
+        # Don't accept excessively long numbers:
+        # def check_rate_length(rate):
+        #     if len(rate) > 7:
+        #         prompt("Please try again. Input should be less than 7 digits.")
+        #         prompt("What is the annual percentage rate (APR)?")
+        #         rate = input_prompt()
+        #         rate = check_rate_length()
+        #         return rate
+        # rate = check_length(rate)
 
         try:
-            # calculate monthly percentage rate
-            return Dec(apr) * Dec(.01) / Dec(12)
+            rate = Dec(rate)
+            # rate should be a positive number or zero:
+            if rate < 0:
+                prompt("Please enter a positive number or zero.")
+                rate = get_apr()
         except decimal.InvalidOperation:
             prompt("Please enter a number in one of the following formats:")
-            prompt("Examples: 5, 5%, 5.2, 5.2%")
-            get_apr()
+            prompt("Examples: 6, 6%, 5.9, 5.9%, 5.99, 5.99%")
+            rate = get_apr()
 
-        return apr
+        # return monthly percentage rate instead of APR:
+        return rate
 
-    mpr = get_apr()
+    monthly_rate = get_apr() * Dec(.01) / Dec(12)
 
     # Ask for loan duration and check inputs for validity:
     def get_loan_duration():
@@ -93,10 +116,10 @@ while True:
         def get_timeframe():
             prompt("Would you like to enter the loan duration in months or "
                    "years?")
-            prompt("Enter 'months' or 'years':")
+            prompt("Enter 'm' for 'months' or 'y' for 'years':")
             timeframe = input_prompt().strip("' ").strip('"').casefold()
 
-            if timeframe not in {'months', 'years'}:
+            if timeframe not in {'months', 'm', 'years', 'y'}:
                 prompt("Please try again.")
                 return get_timeframe()
             return timeframe
@@ -111,19 +134,22 @@ while True:
                 duration = Dec(duration)
                 # don't divide by zero:
                 if duration <= 0:
-                    prompt("Please enter a positive number:")
+                    prompt("Please enter a positive number.")
                     duration = get_duration()
+                if timeframe == 'years':
+                    duration = duration * 12
                 return duration
             except decimal.InvalidOperation:
                 prompt("Please enter a number.")
                 duration = get_duration()
                 return duration
 
-        if timeframe == 'years':
-            return get_duration() * 12
-        return get_duration()
+        if duration > (100 * 12):
+            prompt(f"Maximum loan duration is "
+                   "{'100 years' if timeframe == 'years' else '1200 months'}.")
+            duration = get_duration()
 
-    duration = get_loan_duration()
+    duration_months = get_loan_duration()
 
     def get_balloon():
         pass # return amount or False
@@ -137,7 +163,7 @@ while True:
         payment = amount * (mpr / (1 - (1 + mpr) ** (-duration)))
         return payment
 
-    result = calculate(amount, mpr, duration)
+    monthly_payment = calculate(loan_amount, monthly_rate, duration_months)
 
     # if balloon:
     #     result += balloon
@@ -181,11 +207,11 @@ while True:
         return f"${result}"
 
     prompt("Monthly payment:")
-    prompt(f"{format_result(result)}") # variable name might change
+    prompt(f"{format_result(monthly_payment)}") # variable name might change
     prompt("Total interest:")
-    prompt(f"{format_result(result * duration - amount)}")
-    prompt(f"Total of {duration} payments:")
-    prompt(f"{format_result(result * duration)}")
+    prompt(f"{format_result(monthly_payment * duration_months - loan_amount)}")
+    prompt(f"Total of {duration_months} payments:")
+    prompt(f"{format_result(monthly_payment * duration_months)}")
 
     prompt("Would you like to calculate a new loan? (y/n)")
     answer = input_prompt()
